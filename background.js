5a32d01ef612bc8abfdd5f189f52febf56fcd873
KartikMouli <kartikmouli0@gmail.com>
2024-10-08 20:53:07 +0530

    Add togglebutton feature

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ['contentScript.js']
-    });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['contentScript.js']
   });
-  
+});
